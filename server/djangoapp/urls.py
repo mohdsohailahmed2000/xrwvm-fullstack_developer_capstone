3ed92cdc3dde4a32e4eb2389a1400393cd9400ef
mohdsohailahmed2000 <mohdsohailahmed2000@gmail.com>
2025-01-01 14:45:06 -0500

    Done with Week4

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,23 +5,44 @@ from django.conf import settings
 from . import views
 
 app_name = 'djangoapp'
+
 urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name = 'registration'),
-    # path for login
+    # Path for registration
+    path(route='register', view=views.registration, name='registration'),
+    
+    # Path for login
     path(route='login', view=views.login_user, name='login'),
-
-    #path for logout
-    path(route='logout', view= views.logout_request, name='logout'),
-    # path for dealer reviews view
-    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='get_dealer_reviews'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='get_dealer_reviews'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    # path for add a review view
+    
+    # Path for logout
+    path(route='logout', view=views.logout_request, name='logout'),
+    
+    # Path for dealer reviews view
+    path(
+        route='reviews/dealer/<int:dealer_id>',
+        view=views.get_dealer_reviews,
+        name='get_dealer_reviews'
+    ),
+    
+    # Path for dealer details
+    path(
+        route='dealer/<int:dealer_id>',
+        view=views.get_dealer_details,
+        name='dealer_details'
+    ),
+    
+    # Path for adding a review
     path(route='add_review', view=views.add_review, name='add_review'),
+    
+    # Path for getting all dealerships
     path(route='get_dealers/', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-
+    
+    # Path for getting dealerships by state
+    path(
+        route='get_dealers/<str:state>',
+        view=views.get_dealerships,
+        name='get_dealers_by_state'
+    ),
+    
+    # Path for getting cars
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
