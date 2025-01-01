2207a91a0d8f7bae76095ba61ff1991683945832
mohdsohailahmed2000 <mohdsohailahmed2000@gmail.com>
2025-01-01 14:38:11 -0500

    Done with Week4

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -111,7 +111,7 @@ def initiate():
             "year": 2023,
             "car_make": car_make_instances[4]
         },
-# Add more CarModel instances as needed
+        # Add more CarModel instances as needed
     ]
 
     for data in car_model_data:
