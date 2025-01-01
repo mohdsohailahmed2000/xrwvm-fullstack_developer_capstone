59ddb3c2ef5a989d1fe52ea22746e3e4d494d4a5
mohdsohailahmed2000 <mohdsohailahmed2000@gmail.com>
2025-01-01 13:20:06 -0500

    Done with Week4

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,10 +3,12 @@ import { Routes, Route } from "react-router-dom";
 import Register from "./components/Register/Register";
 import Dealers from './components/Dealers/Dealers';
 import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
     <Routes>
+        <Route path="/postreview/:id" element={<PostReview/>} />
         <Route path="/dealer/:id" element={<Dealer/>} />
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/register" element={<Register />} />  
